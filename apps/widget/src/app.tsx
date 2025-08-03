e8113877919f0adc46f7989a926078d654def7d2
Ricardo Santos <ricardo.santos@ada.tech>
2025-08-03 12:57:02 -0300

    feat: revert pipeline + text for tests

--- apps/widget/src/app.tsx
+++ apps/widget/src/app.tsx
@@ -3,9 +3,6 @@ import { WidgetContainer } from './widget/components/WidgetContainer.tsx';
 function App() {
   return (
     <>
-      <p className="max-w-[50px]">
-        dsadmsaodoas dsadsad dsadsadsa dsadasd adsadas
-      </p>
       <WidgetContainer />
     </>
   );
