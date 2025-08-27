a9b230d6e7c87ad7c8d11e9340584f000fd81d2f
Ricardo Santos <ricardo.santos@ada.tech>
2025-08-27 01:16:09 -0300

    fix: react-query on production

--- apps/widget/src/widget/components/WidgetContainer.tsx
+++ apps/widget/src/widget/components/WidgetContainer.tsx
@@ -1,10 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 import { WidgetContext } from '../lib/context';
 import { Widget } from './Widget';
 
 export function WidgetContainer() {
   const [mounted, setMounted] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const queryClient = new QueryClient();
 
   useEffect(() => {
     setMounted(true);
@@ -16,7 +19,18 @@ export function WidgetContainer() {
 
   return (
     <WidgetContext.Provider value={{ isOpen, setIsOpen }}>
-      <Widget />
+      <QueryClientProvider client={queryClient}>
+        <ToastContainer
+          autoClose={4000}
+          hideProgressBar={true}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <Widget />
+      </QueryClientProvider>
     </WidgetContext.Provider>
   );
 }
