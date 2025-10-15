11158d8db4e1966c0c6e70a5c930469eaac1a28c
Ricardo Santos <ricardo.santos@ada.tech>
2025-10-15 01:44:52 -0300

    feat: remove unnecessary nextjs configs

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  transpilePackages: [
-    '@web-extension-accessibility-frontend/ui',
-    '@web-extension-accessibility-frontend/api-client',
-  ],
-  output: 'standalone',
+  /* config options here */
 };
 
 export default nextConfig;
