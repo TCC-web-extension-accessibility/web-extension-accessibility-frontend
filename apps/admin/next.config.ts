461de80e20c808bbf2c9126cc00c2924547d20b1
Ricardo Santos <ricardo.santos@ada.tech>
2025-10-15 00:28:43 -0300

    fix: move vercel config to root

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: [
+    '@web-extension-accessibility-frontend/ui',
+    '@web-extension-accessibility-frontend/api-client',
+  ],
 };
 
 export default nextConfig;
